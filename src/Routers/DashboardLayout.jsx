331e8bb67808111d25262807248562886f335a6f
Mohammad-Aljada <mohammadaljad.1234@gmail.com>
2025-03-24 01:13:16 +0200

    update final

--- src/Routers/DashboardLayout.jsx
+++ src/Routers/DashboardLayout.jsx
@@ -1,13 +1,13 @@
 import DashboardHeader from '../Components/DashboardHeader';
-import  SideBar  from '../Components/SideBar';
 import { Outlet } from 'react-router';
+import Sidebar from '../Components/Sidebar';
 const DashboardLayout = () => {
     return(
         
     <div className="overflow-hidden pb-2.5 bg-gray-100">
       <div className="flex  max-md:flex-col">
         <aside className="w-[19%] max-md:ml-0 max-md:w-full">
-          <SideBar />
+          <Sidebar />
         </aside>
         <main className=" w-[81%] max-md:ml-0 max-md:w-full">
           <div className="w-full max-md:max-w-full">
